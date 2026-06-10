4b6c2887ea8201b337cd5a642b5fc24b447182d8
Kakagit2025 <humnewsreporter@gmail.com>
2026-06-10 15:44:50 +0530

    Add featured tutors to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+import FeaturedTutors from '../components/FeaturedTutors';
+
 export default function Home() {
   return (
     <main style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>TutorConnect Dehradun</h1>
       <p>Find trusted tutors near you.</p>
-      <a href="/tutors">Browse Tutors</a>
+      <Link href="/tutors">Browse Tutors</Link>
+      <FeaturedTutors />
     </main>
   );
 }
