37db1416950008de9a61d73f4d5ef666dc7c1e3a
Kakagit2025 <humnewsreporter@gmail.com>
2026-06-10 15:37:50 +0530

    Add TutorConnect homepage

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>TutorConnect Dehradun</h1>
+      <p>Find trusted tutors near you.</p>
+      <a href="/tutors">Browse Tutors</a>
+    </main>
+  );
+}
