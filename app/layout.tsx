843c365627be4bd90a10f0803dfc1ac6e9ffe1f2
Kakagit2025 <humnewsreporter@gmail.com>
2026-06-10 15:42:41 +0530

    Integrate Navbar into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from '../components/Navbar';
+
 export const metadata = {
   title: 'TutorConnect Dehradun',
   description: 'Find trusted tutors in Dehradun',
@@ -6,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
